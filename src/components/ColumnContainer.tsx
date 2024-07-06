29371076c782c7ee59dde908f85aef3f583b2c09
Dhanya MD <dhanya13md@gmail.com>
2024-07-07 01:15:26 +0530

    added drag and drop

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,5 +1,7 @@
+import { useSortable } from "@dnd-kit/sortable";
 import TrashIcon from "../icons/TrashIcon";
 import { Column, Id } from "../type";
+import {CSS} from "@dnd-kit/utilities"
   
 interface Props{
     column : Column;
@@ -9,8 +11,34 @@ interface Props{
 export default function ColumnContainer(props : Props){
  const {column, deleteColumn} = props 
 
-    return <div className="bg-columnBackgroundColour w-[350px] h-[500px] max-h-[500px] rounded-lg flex flex-col">
-        <div className="bg-mainBackgroundColour text-md h-[60px] cursor-grab rounded-md rounded-b-none
+  const {setNodeRef, transition, attributes, listeners, transform, isDragging} =  useSortable({
+        id : column.id,
+        data : {
+            type : "Column",
+            column
+        }
+     })
+
+     const style = {
+        transition,
+        transform: CSS.Transform.toString(transform)
+     }
+
+     if(isDragging){
+        return (
+            <div ref={setNodeRef} style={style} 
+            className="bg-columnBackgroundColour w-[350px] h-[500px] max-h-[500px] rounded-lg flex flex-col opacity-40 border-2 border-rose-500">
+    
+            </div>
+        )
+       
+     }
+
+    return <div ref={setNodeRef} style={style} className="bg-columnBackgroundColour w-[350px] h-[500px] max-h-[500px] rounded-lg flex flex-col">
+        <div
+        {...attributes}
+        {...listeners}
+         className="bg-mainBackgroundColour text-md h-[60px] cursor-grab rounded-md rounded-b-none
          p-3 font-semibold border-columnBackgroundColour border-4 flex items-center justify-between">
         <div className="flex gap-2">
         <div className="flex justify-center items-center bg-columnBackgroundColour px-2 py-1 text-sm rounded-full ">0</div>
--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,15 +1,33 @@
-import { useState } from "react"
+import { useState, useMemo } from "react"
 import PlusIcon from "../icons/Plusicon"
 import { Column, Id } from "../type"
 import ColumnContainer from "./ColumnContainer"
+import { DndContext, DragEndEvent, DragOverlay, DragStartEvent, PointerSensor, useSensor, useSensors } from "@dnd-kit/core"
+import { arrayMove, SortableContext } from "@dnd-kit/sortable"
+import { createPortal } from "react-dom"
 
 function KanbanBoard(){
     const[column, setcolumn] = useState<Column[]>([])
-    console.log(column)
+    const columnsId = useMemo(()=> column.map((col)=> col.id),[column])
+    const [activeColumn, setactiveColumn] = useState<Column | null>(null)
+
+    const sensors = useSensors(
+      useSensor(PointerSensor, {
+        activationConstraint: {
+          distance: 10,
+        },
+      })
+    );
+
     return (
         <div className="flex items-center m-auto min-h-screen w-full overflow-x-auto overflow-y-hidden px-[40px]">
+          <DndContext onDragStart={onDragStart} onDragEnd={onDragEnd} sensors={sensors}>
             <div className="m-auto flex gap-2">
-                <div className="flex gap-2">{column.map((col)=> <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn}/>)}</div>
+                <div className="flex gap-2">
+                  <SortableContext items={columnsId}>
+                  {column.map((col)=> <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn}/>)}
+                  </SortableContext>
+                  </div>
             <button  onClick={() => {
                 createNewColumn()
             }} className="h-[60px] w-[300px] min-w-[300px] cursor-pointer text-md rounded-lg bg-mainBackgroundColour border-2 bg-columnBackgroundColour p-4
@@ -17,6 +35,16 @@ function KanbanBoard(){
             <PlusIcon/>
              Add column </button>
             </div>
+            {createPortal (
+              <DragOverlay>
+                {activeColumn && (
+                  <ColumnContainer
+                    column={activeColumn} deleteColumn={deleteColumn} />
+                )}
+              </DragOverlay>,
+              document.body
+            )}
+            </DndContext>
 
             </div>     
     )
@@ -28,6 +56,36 @@ function KanbanBoard(){
     }
     setcolumn([...column, columnToadd])
   } 
+
+  function onDragStart(event : DragStartEvent){
+    console.log("drag start", event)
+    if(event.active.data.current?.type === "Column"){
+      setactiveColumn(event.active.data.current?.column)
+      return
+    }
+  }
+
+  function onDragEnd(event : DragEndEvent){
+   const {active, over} = event;
+
+   const ActiveColumnId = active?.id;
+   const OverColumnId = over?.id;
+
+   const isActiveAColumn = active.data.current?.type === "Column";
+    if (!isActiveAColumn) return;
+
+
+   if(ActiveColumnId === OverColumnId) return 
+
+   setcolumn((columns) => {
+    const activeColumnIndex = columns.findIndex((col) => col.id === ActiveColumnId);
+
+    const overColumnIndex = columns.findIndex((col) => col.id === OverColumnId);
+
+    return arrayMove(columns, activeColumnIndex, overColumnIndex);
+  });
+
+  }
   
   function generateId(){
    return  Math.floor(Math.random()*10001)
@@ -40,3 +98,5 @@ function KanbanBoard(){
 }
 
 export default KanbanBoard
+
+
